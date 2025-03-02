66e37e625c846497fd68d683b6bf42647e6d3f8f
Esthiber <esthiber1@gmail.com>
2025-03-01 20:22:31 -0400

    Creacion del modelo Cuotas

---
+++ GestionPrestamos.Blazor/Models/Cuotas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionPrestamos.Models
+{
+    public class Cuotas
+    {
+        [Key]
+        public int CuotaId { get; set; }
+
+        public int PrestamoId { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El Valor es Requerido.")]
+        public double Valor { get; set; }
+
+        [ForeignKey("PrestamoId")]
+        [InverseProperty("Cuota")]
+        public virtual Prestamos Prestamo { get; set; } = null!;
+    }
+}
