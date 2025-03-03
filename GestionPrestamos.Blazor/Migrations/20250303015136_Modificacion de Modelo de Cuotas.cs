f21a316f6e0a22224f99c067c9677fda51defc1f
Esthiber <esthiber1@gmail.com>
2025-03-02 22:01:25 -0400

    Migracion de correccion de cuotas

---
+++ GestionPrestamos.Blazor/Migrations/20250303015136_Modificacion de Modelo de Cuotas.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionPrestamos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModificaciondeModelodeCuotas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CuotaNo",
+                table: "Cuotas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Cuotas",
+                keyColumn: "CuotaId",
+                keyValue: 1,
+                column: "CuotaNo",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Cuotas",
+                keyColumn: "CuotaId",
+                keyValue: 2,
+                column: "CuotaNo",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CuotaNo",
+                table: "Cuotas");
+        }
+    }
+}
