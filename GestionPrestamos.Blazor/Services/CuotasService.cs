e22b1f86b04638bb4fed296658ce0b702e50fb65
Esthiber <esthiber1@gmail.com>
2025-03-03 00:06:12 -0400

    Creacion del metodo Afectar Cuotas

--- GestionPrestamos.Blazor/Services/CuotasService.cs
+++ GestionPrestamos.Blazor/Services/CuotasService.cs
@@ -81,6 +81,7 @@ namespace GestionPrestamos.Services
         {
             await using var context = await DbFactory.CreateDbContextAsync();
             return await context.CuotasDetalle
+                .Include(c => c.Cuotas)
                 .SingleOrDefaultAsync(c => c.PrestamoId == prestamoId);
         }
 
@@ -112,6 +113,39 @@ namespace GestionPrestamos.Services
             var cuotas = await EliminarListaCuotasAsync(CuotasDetalleId);
             var detalle = await EliminarCuotasDetalleAsync(CuotasDetalleId);
             return cuotas && detalle;
+        }
+
+        public async Task<bool> AfectarCuotas(int PrestamoId, double balance)
+        {
+            if (PrestamoId <= 0 || balance < 0)
+            {
+                throw new ArgumentException("PrestamoId debe ser mayor que 0 y balance no puede ser negativo.");
+            }
+
+            using var context = await DbFactory.CreateDbContextAsync();
+            var detalle = await GetCuotasDetallesAsync(PrestamoId);
+            double restante = balance;
+
+            foreach (var cuota in detalle.Cuotas)
+            {
+                if (cuota.Balance == cuota.Valor) continue;
+
+                if (restante >= cuota.Valor)
+                {
+                    restante -= cuota.Valor;
+                    cuota.Balance = cuota.Valor;
+                }
+                else
+                {
+                    cuota.Balance += restante; 
+                    restante = 0; 
+                    break; 
+                }
+
+                context.Cuotas.Update(cuota);
+            }
+
+            return await context.SaveChangesAsync() > 0;
         }
     }
 }
