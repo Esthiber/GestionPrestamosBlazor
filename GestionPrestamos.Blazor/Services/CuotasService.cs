f419106d799c3459ad3003692a58a9d4ce866dbe
Esthiber <esthiber1@gmail.com>
2025-03-03 00:30:45 -0400

    Actualizacion del metodo afectar cuotas

--- GestionPrestamos.Blazor/Services/CuotasService.cs
+++ GestionPrestamos.Blazor/Services/CuotasService.cs
@@ -115,34 +115,40 @@ namespace GestionPrestamos.Services
             return cuotas && detalle;
         }
 
-        public async Task<bool> AfectarCuotas(int PrestamoId, double balance)
+        public async Task<bool> AfectarCuotas(List<CobrosDetalle> cobrosDetalles)
         {
-            if (PrestamoId <= 0 || balance < 0)
-            {
-                throw new ArgumentException("PrestamoId debe ser mayor que 0 y balance no puede ser negativo.");
-            }
-
             using var context = await DbFactory.CreateDbContextAsync();
-            var detalle = await GetCuotasDetallesAsync(PrestamoId);
-            double restante = balance;
 
-            foreach (var cuota in detalle.Cuotas)
+            foreach (var detalle in cobrosDetalles)
             {
-                if (cuota.Balance == cuota.Valor) continue;
-
-                if (restante >= cuota.Valor)
+                var PrestamoId = detalle.PrestamoId;
+                var balance = detalle.ValorCobrado;
+                if (PrestamoId <= 0 || balance < 0)
                 {
-                    restante -= cuota.Valor;
-                    cuota.Balance = cuota.Valor;
-                }
-                else
-                {
-                    cuota.Balance += restante; 
-                    restante = 0; 
-                    break; 
+                    throw new ArgumentException("PrestamoId debe ser mayor que 0 y balance no puede ser negativo.");
                 }
 
-                context.Cuotas.Update(cuota);
+                var detalleCuotas = await GetCuotasDetallesAsync(PrestamoId);
+                double restante = balance;
+
+                foreach (var cuota in detalleCuotas.Cuotas)
+                {
+                    if (cuota.Balance == cuota.Valor) continue;
+
+                    if (restante >= cuota.Valor)
+                    {
+                        restante -= cuota.Valor;
+                        cuota.Balance = cuota.Valor;
+                    }
+                    else
+                    {
+                        cuota.Balance += restante;
+                        restante = 0;
+                        break;
+                    }
+
+                    context.Cuotas.Update(cuota);
+                }
             }
 
             return await context.SaveChangesAsync() > 0;
